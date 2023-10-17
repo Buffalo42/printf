83e7bb44094916e46b37fa67e42712bdeaacf261
Tigressgee <gogenokulunga@gmail.com>
2023-10-17 16:01:26 -0700

    importance of size and precision

---
+++ size.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * my_size - Calculates size to cast argument
+ * @format: Formatted string to print arguments
+ * @k: Arguments to be printed
+ *
+ * Return: Size
+ */
+
+int my_size(const char *format, int *k)
+{
+	int sip_k = *k + 1;
+	int size = 0;
+
+	if (format[sip_k] == 'l')
+		size = S_LONG;
+	else if (format[sip_k] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*k = sip_k -1;
+	else
+		*k = sip_k;
+
+	return (size);
+}
