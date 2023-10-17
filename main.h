daf7ff18f216026e5eee116c4ad093c88c8f409b
Buffalo42 <atustotle@gmail.com>
2023-10-17 23:20:07 +0100

    main.h

--- main.h
+++ main.h
@@ -4,10 +4,8 @@
 
 
 int _printf(const char *format, ...);
-void print_bin(va_list nc);
+/*void print_bin(va_list nc);*/
 void to_binary(unsigned int nc_num);
-void print_string(va_list  nc);
-void print_char(va_list nc);
 void print_int(va_list nc);
 
 #endif
