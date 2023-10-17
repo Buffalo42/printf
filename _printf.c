19f6ab57c2a523718e52e05df01b352dd217d3fd
Buffalo42 <atustotle@gmail.com>
2023-10-17 21:14:37 +0100

    tasks 0 - 2

--- _printf.c
+++ _printf.c
@@ -7,30 +7,26 @@ int _printf(const char *format, ...);
  * print_char - Print a character.
  *
  * @nc: A va_list containing the character to be printed.
- * @nc_count: A pointer to the character count.
  *
  * Return: the number of charachters printed.
  */
 
-int print_char(va_list nc, int *nc_count)
+void print_char(va_list nc)
 {
 	int c = va_arg(nc, int);
 
-	(*nc_count)++;
 	putchar(c);
-	return (*nc_count);
 }
 
 /**
  * print_string - Print a string.
  *
  * @nc: A va_list containing the string to be printed.
- * @nc_count: A pointer to the character count.
  *
  * Return: void
  */
 
-int print_string(va_list nc, int *nc_count)
+void print_string(va_list nc)
 {
 	const char *s = va_arg(nc, const char *);
 
@@ -39,42 +35,10 @@ int print_string(va_list nc, int *nc_count)
 	while (*s)
 	{
 		putchar(*s);
-		(*nc_count)++;
 		s++;
 	}
-	return (*nc_count);
 }
 
-/**
- * print_percent - Print a percent symbol.
- *
- * @nc_count: A pointer to the character count.
- *
- * Return: void
- */
-int print_percent(int *nc_count)
-{
-	putchar('%');
-	(*nc_count)++;
-	return (*nc_count);
-}
-
-/**
- * print_unknown - Print an unknown format specifier.
- *
- * @nc_count: A pointer to the character count.
- * @format: The format specifier to be printed.
- *
- * Return: void
- */
-int print_unknown(int *nc_count, const char *format)
-{
-	putchar('%');
-	(*nc_count)++;
-	putchar(*format);
-	(*nc_count)++;
-	return (*nc_count);
-}
 
 /**
  * _printf - Custom printf function that produces output according to a format.
@@ -107,23 +71,31 @@ int _printf(const char *format, ...)
 			switch (*format)
 			{
 				case 'c':
-					nc_count += print_char(nc, &nc_count);
+					print_char(nc);
+					nc_count++;
 					break;
 				case 's':
-					nc_count += print_string(nc, &nc_count);
+					print_string(nc);
+					nc_count++;
 					break;
 				case '%':
-					nc_count += print_percent(&nc_count);
+					putchar('%');
+					nc_count++;
 					break;
 				case 'd':
 				case 'i':
-					nc_count += print_int(nc, &nc_count);
+					print_int(nc);
+					nc_count++;
 					break;
 				case 'b':
-					nc_count += print_bin(nc, &nc_count);
+					print_bin(nc);
+					nc_count++;
 					break;
 				default:
-					nc_count += print_unknown(&nc_count, format);
+					putchar('%');
+					nc_count++;
+					putchar(*format);
+					nc_count++;
 					break;
 			}
 		}
--- main.h
+++ main.h
@@ -4,12 +4,10 @@
 
 
 int _printf(const char *format, ...);
-void print_bin(va_list nc, int *nc_count);
+void print_bin(va_list nc);
 void to_binary(unsigned int nc_num);
-void print_unknown(int *nc_count, const char *format);
-void print_percent(int *nc_count);
-void print_string(va_list  nc, int *nc_count);
-void print_char(va_list nc, int *nc_count);
-void print_int(va_list nc, int *nc_count);
+void print_string(va_list  nc);
+void print_char(va_list nc);
+void print_int(va_list nc);
 
 #endif
